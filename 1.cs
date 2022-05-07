45d5f2a22a97c108ea500054580f1cc654bdf874
bokrosf <bokros.feri@hotmail.com>
2022-05-07 20:34:01 +0200

    Problem 1 solved.

---
+++ 1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode;
+
+/// <summary>
+/// https://leetcode.com/problems/two-sum/
+/// </summary>
+public class Solution
+{
+    class IndexRegistry
+    {
+        public int FirstIndex { get; set; }
+        public int? LastIndex { get; set; }
+    }
+
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, IndexRegistry> indeces = GetNumberIndeces(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int difference = target - nums[i];
+
+            if (indeces.TryGetValue(difference, out var registry))
+            {
+                if (difference == nums[i] && registry.LastIndex.HasValue)
+                {
+                    return new int[] { i, registry.LastIndex.Value };
+                }
+                else if (difference != nums[i])
+                {
+                    return new int[] { i, registry.FirstIndex };
+                }
+            }
+        }
+
+        return new int[2];
+    }
+
+    private Dictionary<int, IndexRegistry> GetNumberIndeces(int[] nums)
+    {
+        Dictionary<int, IndexRegistry> indeces = new(nums.Length);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (indeces.TryGetValue(nums[i], out var registry))
+            {
+                registry.LastIndex = i;
+            }
+            else
+            {
+                indeces[nums[i]] = new IndexRegistry { FirstIndex = i, LastIndex = null };
+            }
+        }
+
+        return indeces;
+    }
+}
